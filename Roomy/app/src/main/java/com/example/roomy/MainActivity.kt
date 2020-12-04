e68fa51caadbba7fb1c63f975f8db17b10f94452
Brian Cap <briancap@pop-os.localdomain>
2020-12-03 21:26:32 -0500

    add timber logging

--- Roomy/app/src/main/java/com/example/roomy/MainActivity.kt
+++ Roomy/app/src/main/java/com/example/roomy/MainActivity.kt
@@ -7,20 +7,27 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.databinding.DataBindingUtil
+import com.example.roomy.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //setContentView(R.layout.activity_main)
+        Timber.v( "calling onCreate" )
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main )
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController( R.id.nav_host_fragment )
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications ) )
+
+        setupActionBarWithNavController( navController, appBarConfiguration )
+
+        binding.navView.setupWithNavController( navController )
     }
 }
